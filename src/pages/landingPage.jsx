4462776cf5bfaaf06c2b48d88910407df8261d15
Bhaktij Koli <bhaktijkoli121@gmail.com>
2021-08-21 11:26:42 +0530

    chore: use reactive variable hook

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { useQuery } from '@apollo/client';
+import { useQuery, useReactiveVar } from '@apollo/client';
 import ButtonComponent from '../components/button';
 import { GET_DARK_MODE } from '../graphql/reactivities/theme';
+import { darkMode } from '../graphql/reactivities/theme';
+
 
 const LandingPage = () => {
-
-  const {loading, error, data} = useQuery(GET_DARK_MODE);
-
+  const isdarkMode = useReactiveVar(darkMode)
   return (
-    <div style={{ height: '100vh', backgroundColor: data.darkMode ? 'black' : 'white', color: data.darkMode ? 'white' : 'black' }}>
+    <div style={{ height: '100vh', backgroundColor: isdarkMode ? 'black' : 'white', color: isdarkMode ? 'white' : 'black' }}>
       <h1>Welcome to Theme Toggle Appliation!</h1>
       <ButtonComponent />
     </div>
